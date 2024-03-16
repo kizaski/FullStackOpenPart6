276277cecbb07d9ffcaf851eef2cb5f3c2c8a4be
Kiril <kiril2315@gmail.com>
2024-03-16 22:22:18 +0200

    use createSlice for filter

--- redux-anecdotes/src/reducers/filterReducer.js
+++ redux-anecdotes/src/reducers/filterReducer.js
@@ -1,17 +1,17 @@
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case 'SET_FILTER':
-      return action.payload
-    default:
-      return state
-  }
-}
+import { createSlice } from '@reduxjs/toolkit'
 
-export const filterChange = filter => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter,
-  }
-}
+const initialState = ''
 
-export default filterReducer
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    filterChange(state, action) {
+      const filter = action.payload
+      return filter
+    },
+  },
+})
+
+export const { filterChange } = filterSlice.actions
+export default filterSlice.reducer
