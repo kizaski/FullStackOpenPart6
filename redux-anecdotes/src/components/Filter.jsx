3d23c72abc9e44e19f8d3366c15d2675999f904f
Kiril <kiril2315@gmail.com>
2024-03-16 21:17:36 +0200

    6.9 filter works

---
+++ redux-anecdotes/src/components/Filter.jsx
@@ -0,0 +1,24 @@
+import { useDispatch } from 'react-redux'
+import { filterChange } from '../reducers/filterReducer'
+
+const Filter = () => {
+    // const filter = useSelector(state => state.filter)
+    const dispatch = useDispatch()
+
+    const handleChange = (event) => {
+      event.preventDefault()
+      // input-field value is in variable event.target.value
+      dispatch(filterChange(event.target.value))
+    }
+    const style = {
+      marginBottom: 10
+    }
+  
+    return (
+      <div style={style}>
+        filter <input onChange={handleChange} />
+      </div>
+    )
+  }
+  
+  export default Filter
