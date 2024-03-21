94bb61b3720c0fb33de6a8ee34ca3b3c43c5b227
Kiril <kiril2315@gmail.com>
2024-03-21 18:39:15 +0200

    init NotificationContextProvider

---
+++ query-anecdotes/src/NotificationContext.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable react-refresh/only-export-components */
+/* eslint-disable react/prop-types */
+import { createContext, useReducer, useContext  } from 'react'
+
+const notificationReducer = (state, action) => {
+  switch (action.type) {
+    case "INC":
+        return state + 1
+    case "DEC":
+        return state - 1
+    case "ZERO":
+        return 0
+    default:
+        return state
+  }
+}
+
+const NotificationContext = createContext()
+
+export const NotificationContextProvider = (props) => {
+  const [Notification, NotificationDispatch] = useReducer(notificationReducer, 0)
+
+  return (
+    <NotificationContext.Provider value={[Notification, NotificationDispatch] }>
+      {props.children}
+    </NotificationContext.Provider>
+  )
+}
+
+export const useNotificationValue = () => {
+    const notificationAndDispatch = useContext(NotificationContext)
+    return notificationAndDispatch[0]
+}
+
+export const useNotificationDispatch = () => {
+    const notificationAndDispatch = useContext(NotificationContext)
+    return notificationAndDispatch[1]
+}
+
+export default NotificationContext
