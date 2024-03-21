3c69fb21144fabd746a2b109f6341eebc5e0cf05
Kiril <kiril2315@gmail.com>
2024-03-21 17:57:57 +0200

    init ReactQuery

--- query-anecdotes/src/requests.js
+++ query-anecdotes/src/requests.js
@@ -1,4 +1,4 @@
 import axios from 'axios'
 
 export const getAnecdotes = () =>
-  axios.get('http://localhost:3001/notes').then(res => res.data)
+  axios.get('http://localhost:3001/anecdotes').then(res => res.data)
